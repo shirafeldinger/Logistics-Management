80080cfa0d182905c8df42e345df71c81fb6773c
Shira Feldinger <shirafeldinger@gmail.com>
2020-07-25 15:38:30 +0300

    Updates

---
+++ precache-manifest.ea43ebb0b0d1065f1ce3e663ab05bcb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e6a03613c6243f576f44114849f7e13",
+    "url": "/Logistics-Management/index.html"
+  },
+  {
+    "revision": "0b1b7f810d3c8ce540d3",
+    "url": "/Logistics-Management/static/css/main.d97f5384.chunk.css"
+  },
+  {
+    "revision": "6a5f2e5019aa0305599e",
+    "url": "/Logistics-Management/static/js/2.ee69a78d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Logistics-Management/static/js/2.ee69a78d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b1b7f810d3c8ce540d3",
+    "url": "/Logistics-Management/static/js/main.a56c2e94.chunk.js"
+  },
+  {
+    "revision": "d8a1f1caf1a5bb5fdd7f",
+    "url": "/Logistics-Management/static/js/runtime-main.4f9403ea.js"
+  }
+]);
